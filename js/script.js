44d9ea518c7c981ee8a153f5d7b2eef2bc9ba7d4
Sakshimathur749 <mathursakshi749@gmail.com>
2026-08-24 11:26:41 +0530

    changes in js

--- js/script.js
+++ js/script.js
@@ -96,20 +96,4 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize first card active & start autoplay
     activateCard(0);
     startAutoplay();
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const admissionForm = document.getElementById("admissionForm");
-
-    if (!admissionForm) return;
-
-    document.querySelectorAll("button").forEach(function (button) {
-        button.addEventListener("click", function (e) {
-            e.preventDefault();
-
-            admissionForm.scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        });
-    });
-});
+}); 
